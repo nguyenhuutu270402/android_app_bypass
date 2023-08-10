4eb1c455537cf8759e15e5b6c7bf2049a00f30bb
Nguyễn Hữu Tứ <huutu0427@gmail.com>
2023-08-10 11:44:45 +0700

    update1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val packageManager: PackageManager = packageManager
+        val installedPackages: List<PackageInfo> = packageManager.getInstalledPackages(0)
+
+        val installedApps: MutableList<ApplicationInfo> = mutableListOf()
+
+        println(">>>>>>>>>>> 0000000000000000000000000")
+        for (packageInfo in installedPackages) {
+            val appInfo: ApplicationInfo = packageInfo.applicationInfo
+            println(
+                ">>>>>>>>>>> appInfo.loadLabel(packageManager) : ${
+                    appInfo.loadLabel(
+                        packageManager
+                    )
+                }"
+            )
+            println(">>>>>>>>>>> name : ${appInfo.name}")
+            println(">>>>>>>>>>> enabled : ${appInfo.enabled} \n")
+
+//            if (appInfo.name != null) {
+//                println(">>>>>>>>>>> ${appInfo.name}")
+//                println(">>>>>>>>>>> ${appInfo.loadLabel(packageManager).toString()}")
+//            }
+
+
+            installedApps.add(appInfo)
+        }
     }
 }
